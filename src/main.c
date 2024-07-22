741e9819ae51b743c62d99ac1e96fe58c7fa1e09
Vladimir Kupryukhin <45701631+VladimirKupryukhin@users.noreply.github.com>
2024-07-22 14:22:10 -0700

    Vlad/setup servo control (#1)
    * Servo motor pwm code. Needs testings
    
    * Servo control with a target angle WORKS WOOO

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "pico/stdlib.h"
+#include "../include/servo.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -15,6 +16,27 @@ int main(int argc, char** argv) {
         gpio_put(25,index % 2);
         sleep_ms(1000);
         printf("%d \n", index);
+    }
+
+
+    struct ServoMotor* turntableMotor = createServoMotor(15);
+    struct ServoMotor* yawMotor = createServoMotor(14);
+
+    int angle = 10;//idk 0 kinda bends the thing
+    int step = 10;
+
+    while(true){
+        printf("angle: %f\n", turntableMotor->currentAngle);
+        turntableMotor->setAngle(turntableMotor, angle);
+        yawMotor->setAngle(yawMotor, angle);
+
+        angle += step;
+
+        if (angle > 180 || angle < 10) {
+            step *= -1;
+        }
+
+        sleep_ms(200);
     }
 
     return 0;
---
+++ src/servo.c
@@ -0,0 +1,69 @@
+#include "../include/servo.h"
+
+
+struct ServoMotor* createServoMotor(int gpioPinPWM) {
+
+
+    struct ServoMotor* motorObject = (struct ServoMotor*) malloc(sizeof(struct ServoMotor));
+
+
+    // set the function pointers
+    motorObject->setAngle = &setAngle;
+    motorObject->degree2radian = &degree2radian;
+    motorObject->radian2degree = &degree2radian;
+
+
+    // set the variables
+    motorObject->gpioPinPWM = gpioPinPWM;
+    motorObject->currentDuty = 0;
+    motorObject->currentAngle = 0;
+
+
+    // initialize the gpio pin for pwm
+    gpio_set_function(motorObject->gpioPinPWM, GPIO_FUNC_PWM);
+
+    motorObject->gpioSlice = pwm_gpio_to_slice_num(motorObject->gpioPinPWM);
+    motorObject->gpioChannel = pwm_gpio_to_channel(motorObject->gpioPinPWM);
+
+    // This makes the counter go up every 250th clock cycle
+    // thus giving us a frequency of 500kHz
+    pwm_set_clkdiv(motorObject->gpioSlice, 250.0);
+
+    // This will make the TOP value be 10,000
+    // Meaning that the counter will reset every 10,000th
+    // counter value
+    // This gives us a frequency of 50Hz
+    // 10,000 correlates to 100% duty cycle
+    pwm_set_wrap(motorObject->gpioSlice, 10000);
+
+    // Default position is in the middle, meaning 90 degrees
+    motorObject->setAngle(motorObject, 50.0);
+
+    pwm_set_enabled(motorObject->gpioSlice, true);
+
+    return motorObject;
+}
+
+
+void setAngle(struct ServoMotor* object, double target) {
+    printf("here\n");
+    object->currentAngle = target;
+    int newTargetLevel = 5.555 * target + 250;
+    printf("now here\n");
+    pwm_set_chan_level(object->gpioSlice, object->gpioChannel, newTargetLevel);
+}
+
+double degree2radian(double degree){
+
+    return 0.0;
+}
+
+double radian2degree(double radian){
+    return 0.0;
+}
+
+
+
+
+
+
